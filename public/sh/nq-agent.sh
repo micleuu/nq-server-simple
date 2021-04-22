ccbc00bbc9ba6ef3567592ccbf8fcbe1e82ba11b
micleuu <45659357+micleuu@users.noreply.github.com>
2021-04-23 03:24:13 +0800

    Update nq-agent.sh

--- public/sh/nq-agent.sh
+++ public/sh/nq-agent.sh
@@ -239,9 +239,9 @@ data_post="token=${auth[0]}&data=$(base "$version") $(base "$uptime") $(base "$s
 # API request with automatic termination
 if [ -n "$(command -v timeout)" ]
 then
-	timeout -s SIGKILL 30 wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "http://www.monitorx.xyz/api/agent.json"
+	timeout -s SIGKILL 30 wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "http://10.10.10.41/api/agent.json"
 else
-	wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "http://www.monitorx.xyz/api/agent.json"
+	wget -q -o /dev/null -O /etc/nodequery/nq-agent.log -T 25 --post-data "$data_post" --no-check-certificate "http://10.10.10.41/api/agent.json"
 	wget_pid=$! 
 	wget_counter=0
 	wget_timeout=30
